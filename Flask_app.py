359aaef22d572358c6a6b59882e537c33635bea7
Aytun <aytuncetinkaya@outlook.com>
2023-08-19 11:22:14 +0200

    Basic Flask code

--- Flask_app.py
+++ Flask_app.py
@@ -2,10 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
-if __name__ == '__main__':    
-  app.run(port=5000,debug=True)
+
 
 @app.route('/')
 def home():     
     return "<p>Dit is mijn eerste site met Flask.</p>"
 
+if __name__ == '__main__':    
+  app.run(port=5000,debug=True)
